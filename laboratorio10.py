df4531be4a4bc13e97933b9a4ba077bf8e132a8b
JuanRojasUpb <62483681+JuanRojasUpb@users.noreply.github.com>
2020-04-12 16:06:04 -0500

    modificacion con multiples a de entrada

--- laboratorio10.py
+++ laboratorio10.py
@@ -7,12 +7,17 @@ Created on Sat Apr 11 12:29:41 2020
 
 
 def a_power_b(a,b,resultado):
-    for i in range(b):
+    if a == 0:
+        print("no esta definida")
+    else:
+            
+     for i in range(b):
         resultado = resultado*a
     return resultado
 
 a = int(input("ingrese el numero base"))
 b = int(input("ingrese el numero de exponente"))
+    
 resultado = 1
 
 print("la potencia es:" + str (a_power_b(a,b,resultado)))
